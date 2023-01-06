ce79b9a5cca1d43f66f721d3faa780789c690dfc
zidanegimiga <zidanegimiga@gmail.com>
2023-01-06 12:07:28 +0300

    fix: fix build error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,6 @@
 export default function Index() {
-  const height = window.innerHeight;
-  const width = window.innerWidth;
-
-  return (
-    <div>
-      {
-        if(window){
-          <div>
-            width: {width}
-            height: {height}
-          </div>
-        }
-      }
-    </div>
-  );
+    return (
+      <div>
+      </div>
+    );
 }
