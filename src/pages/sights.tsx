c4c9805141d7609249e530fc9ed41c10d3824965
zidanegimiga <zidanegimigatemp@gmail.com>
2023-01-12 11:40:28 +0300

    feat: add mock data and creat sights page

---
+++ src/pages/sights.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from '../styles/Sights.module.scss';
+
+const sights = () => {
+    return (
+        <div className={styles.pageWrapper}>
+            <div className={styles.hero}>
+                <div className={styles.posterWrapper}>
+                    <img />
+                </div>
+                <div className={styles.detailsWrapper}></div>
+            </div>
+        </div>
+    )
+}
+
+export default sights;
