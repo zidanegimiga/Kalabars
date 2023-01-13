bf1120e2aa8e787e8e9f15869380f1a7f4c8cb9e
zidanegimiga <zidanegimiga@gmail.com>
2023-01-13 12:46:31 +0300

    feat: add components to sights page

--- src/pages/sights.tsx
+++ src/pages/sights.tsx
@@ -1,24 +1,33 @@
-import MovieCarousel from 'features/Sights/MovieCarousel';
-import {useState} from 'react'
-import styles from '../styles/Sights.module.scss';
+import MovieCarousel from "features/Sights/MovieCarousel";
+import SightsCard from "features/Sights/SightsCard/SightsCard";
+import SightsCategory from "features/Sights/SightsCategory";
+import { useState } from "react";
+import Down from "../../public/downIcon.svg";
+import styles from "../styles/Sights.module.scss";
 
 const mockData = [
-    {
-        name: "Alice",
-        poster: "/alice.png"
-    },
-    {
-        name: "Black Valentine",
-        poster: "/blackValentine.png"
-    },
-]
+  {
+    name: "Alice",
+    poster: "/alice.png",
+  },
+  {
+    name: "Black Valentine",
+    poster: "/blackValentine.png",
+  },
+];
 
 const sights = () => {
-    return (
-        <div className={styles.pageWrapper}>
-            <MovieCarousel />
-        </div>
-    )
-}
+  return (
+    <div className={styles.pageWrapper}>
+      <MovieCarousel />
+      <SightsCategory name={"Staff Picks"}>
+        <SightsCard />
+        <SightsCard />
+        <SightsCard />
+        <SightsCard />
+      </SightsCategory>
+    </div>
+  );
+};
 
 export default sights;
