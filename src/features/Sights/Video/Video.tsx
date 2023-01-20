b0cbc9568e6b9bb565eb2786a2efd7f5a334fa40
zidanegimiga <zidanegimigatemp@gmail.com>
2023-01-20 17:00:49 +0300

    setVideoTime

--- src/features/Sights/Video/Video.tsx
+++ src/features/Sights/Video/Video.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import {
   Play,
   Pause,
@@ -20,24 +20,59 @@ const VideoPlayer = () => {
   const [muted, setMuted] = useState(false);
   const videoRef = useRef(null);
   const videoContainer = useRef(null);
+  const timeline = useRef(null);
 
-  console.log("current time: ", progress);
+  useEffect(() => {
+      // setVideoTime(videoRef.current.duration);
+      // setInterval(function () {
+      //   setCurrentTime(videoRef.current.currentTime);
+      //   setProgress((videoRef.current.currentTime / videoTime) * 100);
+      //   timeline.current.style.setProperty("--progress-position", progress);
+      //   console.log("Progress: ", progress)
+      // }, 1000);
+      const videodata = fetch('"https://content.kalabars.com/videos/all', {
+        headers:{
+          'x-access-token': "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwdWJsaWNfaWQiOiIwOWVhYjVhOC04ZTEyLTRrMGItYTkyNi1hZmZmMGM1NDNhMjQiLCJleHAiOjE2NzY4NDQ0Mzd9.0QVC_lMKtroGKRMp3_hjf2cLbaGvOt3G0FJXdnTDwZw"
+        } 
+      }).then((data) => console.log("fetch: ",data));
+      console.log("video: ", videodata)
+  }, []);
 
-  if (typeof window !== "undefined") {
-    window.setInterval(function () {
-      setCurrentTime(videoRef.current?.currentTime);
-      setProgress((videoRef.current?.currentTime / videoTime) * 100);
-    }, 1000);
-  }
 
-  //play
+
+  const handleTimelineUpdate = (e) => {
+    e.preventDefault();
+    const parentRect = timeline.current.getBoundingClientRect();
+
+    //calculating the position of our mouse relative to the timeline
+    const percent = Math.min(Math.max(0, e.clientX - parentRect.x)) / parentRect.width;
+    timeline.current.style.setProperty("--preview-position", percent)
+    console.log("Timeline Events ", parentRect.width);
+  };
+
+  const leadingZerosFormatter = new Intl.NumberFormat(undefined, {
+    minimumIntegerDigits: 2,
+  });
+
+  const formatDuration = (time) => {
+    const seconds = Math.floor(time % 60);
+    const minutes = Math.floor(time % 60) % 60;
+    const hours = Math.floor(time % 3600);
+
+    if (hours === 0) {
+      return `${minutes} : ${leadingZerosFormatter.format(seconds)}`;
+    } else {
+      return `${hours}: ${leadingZerosFormatter.format(
+        minutes
+      )} : ${leadingZerosFormatter.format(seconds)}`;
+    }
+  };
+
+  //play/pause
   const videoHandler = (control) => {
     if (control === "play") {
       videoRef.current.play();
       setPlaying(true);
-      // var vid = document.getElementById("video1");
-      // setVideoTime(vid?.duration);
-      setVideoTime(videoRef.current.duration);
     } else if (control === "pause") {
       videoRef.current.pause();
       setPlaying(false);
@@ -53,10 +88,16 @@ const VideoPlayer = () => {
     videoRef.current.currentTime -= 5;
   };
 
+  //screen
   const pictureInPicture = (e) => {
     videoRef.current.requestPictureInPicture();
   };
 
+  const fullScreen = () => {
+    videoRef.current.requestFullscreen();
+  };
+
+  //volume
   const handleVolumeChange = (e) => {
     videoRef.current.volume = e.target.value / 100;
     setVolume(Number(e.target.value));
@@ -80,15 +121,6 @@ const VideoPlayer = () => {
     setVolume(Math.floor(videoRef.current.volume * 100));
   };
 
-  const fullScreen = () =>{
-    videoRef.current.requestFullscreen();
-    if(document.fullscreenElement) {
-      document.exitFullscreen();
-  } else {
-      videoContainer.current.requestFullscreen();
-  }
-  }
-
   return (
     <div className={styles.videoWrapper} ref={videoContainer}>
       <video
@@ -98,6 +130,8 @@ const VideoPlayer = () => {
         onClick={() => {
           playing ? videoHandler("pause") : videoHandler("play");
         }}
+        onLoadedData={() => formatDuration(videoRef.current.duration)}
+        onTimeUpdate={() => formatDuration(videoRef.current.currentTime)}
         className={styles.video}
         src="https://media.geeksforgeeks.org/wp-content/uploads/20190616234019/Canvas.move_.mp4"
         // src="/sample.mp4"
@@ -106,8 +140,32 @@ const VideoPlayer = () => {
         {/* <source src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" type="video/mp4" /> */}
       </video>
       <div className={styles.controlsContainer}>
+        <div className={styles.timeControls}>
+          <p className={styles.time}>
+            {Math.floor(currentTime / 60) +
+              ":" +
+              ("0" + Math.floor(currentTime % 60)).slice(-2)}
+          </p>
+          <div 
+            className={styles.timelineContainer}
+            onMouseMove={(e) => handleTimelineUpdate(e)}
+            ref={timeline}
+          >
+            <div
+              className={styles.timeline}
+            >
+              <img className={styles.previewImg} src={"/preview.png"} alt="preview"/>
+              <div className={styles.thumbIndicator}></div>
+            </div>
+          </div>
+          <p className={styles.time}>
+            {Math.floor(videoTime / 60) +
+              ":" +
+              ("0" + Math.floor(videoTime % 60)).slice(-2)}
+          </p>
+        </div>
         <div className={styles.playbackIcons}>
-          <div className={styles.volumeIcon}>
+          <div className={styles.volumeIconContainer}>
             <div className={styles.controlIconVolume}>
               {muted === true ? (
                 <VolumeOff action={handleVolumeUnmute} />
@@ -158,30 +216,6 @@ const VideoPlayer = () => {
               <FullScreen action={fullScreen} />
             </div>
           </div>
-        </div>
-        <div className={styles.timeControls}>
-          <p className={styles.time}>
-            {Math.floor(currentTime / 60) +
-              ":" +
-              ("0" + Math.floor(currentTime % 60)).slice(-2)}
-          </p>
-          <div className={styles.timeProgressBarContainer}>
-            {/* <div style={{ width: `${progress}%`}}  className={styles.timeProgressBar}></div> */}
-            <input
-              type="range"
-              value={progress}
-              min="1"
-              max="100"
-              className={styles.timeProgressBar}
-              id="myRange"
-            ></input>
-            {/* <progress value={progress} max="100" className={styles.timeProgressBar} style={{ width: `${progress}%`}}/> */}
-          </div>
-          <p className={styles.time}>
-            {Math.floor(videoTime / 60) +
-              ":" +
-              ("0" + Math.floor(videoTime % 60)).slice(-2)}
-          </p>
         </div>
       </div>
       Playing: {playing ? "true" : "false"}
