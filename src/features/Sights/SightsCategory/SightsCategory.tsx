9d6e2c713926a6aa40043c9f07758a6e93dc93c9
zidanegimiga <zidanegimigatemp@gmail.com>
2023-01-31 17:02:11 +0300

    feat: remove children from sights category

--- src/features/Sights/SightsCategory/SightsCategory.tsx
+++ src/features/Sights/SightsCategory/SightsCategory.tsx
@@ -3,7 +3,7 @@ import styles from "./SightsCategory.module.scss";
 import Down from "../../../../public/downIcon.svg";
 import SightsCard from "../SightsCard/";
 
-const SightsCategory = ({children, name}) => {
+const SightsCategory = ({name, data}) => {
   return (
     <div className={styles.sightsCategory}>
       <div className={styles.top}>
@@ -13,7 +13,13 @@ const SightsCategory = ({children, name}) => {
         </div>
       </div>
       <div className={styles.categoryCards}>
-        {children}
+        {
+          data?.map((video, index) => (
+            <div key={index}>
+              <SightsCard cardData={video}/>
+            </div>
+          ))          
+        }
       </div>
     </div>
   );
