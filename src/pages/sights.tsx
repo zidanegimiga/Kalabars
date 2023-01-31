721943415fe539e98dc304c18639624418db5a71
zidanegimiga <zidanegimigatemp@gmail.com>
2023-01-31 17:00:26 +0300

    feat: ADD CATEGORIES FOR VIDEOS

--- src/pages/sights.tsx
+++ src/pages/sights.tsx
@@ -1,31 +1,24 @@
 import MovieCarousel from "features/Sights/MovieCarousel";
 import SightsCard from "features/Sights/SightsCard/SightsCard";
 import SightsCategory from "features/Sights/SightsCategory";
+import { dirname } from "path";
 import { useState, useRef } from "react";
 import Down from "../../public/downIcon.svg";
 import styles from "../styles/Sights.module.scss";
 
-const Sights = ({ videos, genres, eg }) => {
-  const pager = useRef(null)
-  // console.log("Videos: ", videos?.response);
-  const carouselVideos = videos.response.result.slice(18, 26);
-  const pageGenres = genres.response.result;
-  console.log("Videos: ", videos.response.result);
-  console.log("Pager: ", pageGenres);
-  console.log("Genres: ", eg);
-
-  const videosByGenre = () =>{
-  }
+const Sights = ({ videos, drama, originals, comedy, documentary, staffPicks}) => {
+  const pager = useRef(null);
+  const carouselVideos = videos.response.result.slice(15, 24);
+  console.log("Food: ", documentary?.response?.result)
+  
 
   return (
     <div className={styles.pageWrapper} ref={pager}>
-      <MovieCarousel videos={carouselVideos}/>
-      <SightsCategory name={"Staff Picks"}>
-        {/* <SightsCard />
-        <SightsCard />
-        <SightsCard />
-        <SightsCard /> */}
-      </SightsCategory>
+      <MovieCarousel videos={carouselVideos} />
+      <SightsCategory name={"Staff Picks"} data={staffPicks.response.result} />  
+      <SightsCategory name={"Kalabars Originals"} data={originals.response.result} />
+      <SightsCategory name={"Comedy"} data={comedy?.response?.result} />      
+      <SightsCategory name={"Drama"} data={drama?.response?.result} />              
     </div>
   );
 };
@@ -33,33 +26,97 @@ const Sights = ({ videos, genres, eg }) => {
 export default Sights;
 
 export async function loadVideos() {
-  const resVideos = await fetch(process.env.API+`/videos/all`, {
-    headers:{
-      'x-access-token': process.env.TOKEN 
-    }});
-  const resGenres = await fetch(process.env.API+`/videos/genres`, {
-    headers:{
-      'x-access-token': process.env.TOKEN 
-  }});
-  const kalabarsGenres = await fetch(process.env.API+`/genres/kalabars-originals/videos`, {
-    headers:{
-      'x-access-token': process.env.TOKEN 
-  }});
-  const genres = await resGenres.json();
-  const exampleGenres = await kalabarsGenres.json();
+  //All Videos  
+  const resVideos = await fetch(process.env.API + `/videos/all`, {
+    headers: {
+      "x-access-token": process.env.TOKEN,
+    },
+  });
   const videos = await resVideos.json();
+  
+  //originals
+  const originalsGenres = await fetch(
+    process.env.API + `/genres/kalabars-originals/videos`,
+    {
+      headers: {
+        "x-access-token": process.env.TOKEN,
+      },
+    }
+  );
+  const originals = await originalsGenres.json();
+  
+  //Drama
+  const dramaGenres = await fetch(
+    process.env.API + `/genres/drama/videos`,
+    {
+      headers: {
+        "x-access-token": process.env.TOKEN,
+      },
+    }
+  );
+  const dramaGenre = await dramaGenres.json();
+
+  //Documentary
+  const documentaryGenres = await fetch(
+    process.env.API + `/genres/documentaries/videos`,
+    {
+      headers: {
+        "x-access-token": process.env.TOKEN,
+      },
+    }
+  );
+  const documentaryGenre = await documentaryGenres.json();
+
+  //Comedy
+  const comedyGenres = await fetch(
+    process.env.API + `/genres/comedy/videos`,
+    {
+      headers: {
+        "x-access-token": process.env.TOKEN,
+      },
+    }
+  );
+  const comedyGenre = await comedyGenres.json(); 
+
+  //Comedy
+  const staffPicksGenres = await fetch(
+    process.env.API + `/tags/staff-picks/videos`,
+    {
+      headers: {
+        "x-access-token": process.env.TOKEN,
+      },
+    }
+  );
+  const staffPicks = await staffPicksGenres.json();  
+
   return {
-    genres: genres,
     videos: videos,
-    eg: exampleGenres
+    originals: originals,
+    comedy: comedyGenre,
+    documentary: documentaryGenre,
+    drama: dramaGenre,
+    staffPicks: staffPicks
   };
 }
+const genres = [
+  "drama",
+  "series",
+  "in-session",
+  "jams",
+  "thriller",
+  "jams",
+  "local",
+  "food",
+  "comedy",
+  "poetic",
+];
+
 
 export async function getStaticProps(context) {
-  const {genres, videos, eg} = await loadVideos();
+  const { videos, originals, comedy, documentary, drama, staffPicks } = await loadVideos();
   return {
     props: {
-      genres, videos, eg
+      videos, originals, documentary, drama, comedy, staffPicks
     },
   };
 }
