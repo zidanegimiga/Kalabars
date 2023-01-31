ee667d35b2131c5619ab1aecc9c27244bb1e75d3
zidanegimiga <zidanegimigatemp@gmail.com>
2023-01-31 17:03:05 +0300

    refactor: make movie carousel code prettier

--- src/features/Sights/MovieCarousel/MovieCarousel.tsx
+++ src/features/Sights/MovieCarousel/MovieCarousel.tsx
@@ -37,29 +37,44 @@ const Hero = ({ videos }) => {
           );
         }}
       >
-        {videos.map((video, index) => (
-          <div className={styles.hero} key={index}>
-            <Image width={1920} height={1400} src={`https:content.kalabars.com/static/media/videos_images/`+video.landscape_image} alt="poster" className={styles.poster} />
+        {videos.map(function (video, index) {
+          if(video.landscape_image === null){
+            return null;
+          }
+          return (
+            <div className={styles.hero} key={index}>
+            <Image
+              width={1920}
+              height={1400}
+              src={
+                `https:content.kalabars.com/static/media/videos_images/` +
+                video.landscape_image
+              }
+              alt="poster"
+              className={styles.poster}
+            />
             <div className={styles.detailsWrapper}>
               <div className={styles.detailsContainer}>
                 <div className={styles.title}>{video.title}</div>
-                <div className={styles.description}>
-                  {video.description}
-                </div>
+                <div className={styles.description}>{video.description}</div>
                 <div className={styles.tags}>
                   <div className={styles.tag}>THRILLER</div>
-                  {video.genre?.length !== 0 && 
-                    video.genre?.map((tag, index)=>(
-                      <div className={styles.tag} key={index}>tag</div>
-                    ))
-                  }
+                  {video.genre?.length !== 0 &&
+                    video.genre?.map((tag, index) => (
+                      <div className={styles.tag} key={index}>
+                        tag
+                      </div>
+                    ))}
                   <div className={styles.time}>
                     <Timer className={styles.timeicon} />
                     <p>{video.duration} min</p>
                   </div>
                 </div>
                 <div className={styles.buttonsContainer}>
-                  <div className={styles.button1} onClick={()=>router.push(`/videos/${video.public_id}`)}>
+                  <div
+                    className={styles.button1}
+                    onClick={() => router.push(`/videos/${video.public_id}`)}
+                  >
                     {" "}
                     <Play className={styles.playicon} />
                     <p>Start Watching</p>
@@ -72,7 +87,8 @@ const Hero = ({ videos }) => {
               </div>
             </div>
           </div>
-        ))}
+        )
+        })}
       </Carousel>
     </div>
   );
