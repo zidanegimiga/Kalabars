4c525f26166e8d230f64b1fd01820abfbe164fd7
zidanegimiga <zidanegimigatemp@gmail.com>
2023-02-13 13:43:35 +0300

    styles: improve sights card aesthetics

--- src/features/Sights/SightsCard/SightsCard.tsx
+++ src/features/Sights/SightsCard/SightsCard.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 import styles from "./SightsCard.module.scss";
+import Link from 'next/link';
 
 const SightsCard = ({cardData}) => {
-  const router = useRouter();
-  function handleRoute(){
-    router.push("/video");
-  }
     
   //Some videos lack images
   if(cardData?.square_image === null){
@@ -14,11 +11,14 @@ const SightsCard = ({cardData}) => {
   }
 
   return (
-    <div className={styles.categoryCardWrapper} onClick={()=>router.push(`/videos/${cardData?.public_id}`)}>
-    {/* <div className={styles.categoryCardWrapper} onClick={()=>router.push(`/videos/9186eefa-e7d4-45e1-a34b-d4bb6bd7e799`)}> */}
-      <img src={`https:content.kalabars.com/static/media/videos_images/${cardData?.square_image}`} alt="toilet chronicles" className={styles.cardImage}/>
-      <div className={styles.movieTitle}>{cardData?.title}</div>
-    </div>
+	<Link href={`/videos/${cardData?.public_id}`}>
+		<div className={styles.categoryCardWrapper}>
+			{/* <div className={styles.categoryCardWrapper} onClick={()=>router.push(`/videos/9186eefa-e7d4-45e1-a34b-d4bb6bd7e799`)}> */}
+			<img src={`https:content.kalabars.com/static/media/videos_images/${cardData?.square_image}`} alt="toilet chronicles" className={styles.cardImage}/>
+			<div className={styles.movieTitle}>{cardData?.title}</div>
+			<Link href={`/videos/${cardData?.public_id}`}><div className={styles.cardBtn}>Watch</div></Link>
+		</div>
+	</Link>
   )
 }
 
