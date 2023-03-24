1ff2c628a26885ab3956f7a523abf6e15cf11434
zidanegimiga <zidanegimigatemp@gmail.com>
2023-03-24 14:53:52 +0300

    feat: add new play icon and set hover states

--- src/features/Sights/SightsCard/SightsCard.tsx
+++ src/features/Sights/SightsCard/SightsCard.tsx
@@ -1,26 +1,40 @@
 /* eslint-disable @next/next/no-img-element */
-import React from 'react';
-import { useRouter } from 'next/router';
+import React, { useState } from "react";
+import { useRouter } from "next/router";
 import styles from "./SightsCard.module.scss";
-import Link from 'next/link';
+import Link from "next/link";
+import { PlayIcon } from "shared/Icons/Playback";
 
-const SightsCard = ({cardData}) => {
-    
+const SightsCard = ({ cardData }) => {
+  const [hovered, setHovered] = useState(false);
   //Some videos lack images
-  if(cardData?.square_image === null){
+  if (cardData?.square_image === null) {
     return null;
   }
 
   return (
-	<Link href={`/videos/${cardData?.public_id}`}>
-		<div className={styles.categoryCardWrapper}>
-			{/* <div className={styles.categoryCardWrapper} onClick={()=>router.push(`/videos/9186eefa-e7d4-45e1-a34b-d4bb6bd7e799`)}> */}
-			<img src={`${process.env.NEXT_PUBLIC_API}/static/media/videos_images/${cardData?.square_image}`} alt={cardData?.title} className={styles.cardImage}/>
-			<div className={styles.movieTitle}>{cardData?.title}</div>
-			<Link href={`/videos/${cardData?.public_id}`}><div className={styles.cardBtn}>Watch</div></Link>
-		</div>
-	</Link>
-  )
-}
+    <Link href={`/videos/${cardData?.public_id}`}>
+      <div className={styles.categoryCardWrapper}>
+        {/* <div className={styles.categoryCardWrapper} onClick={()=>router.push(`/videos/9186eefa-e7d4-45e1-a34b-d4bb6bd7e799`)}> */}
+        <img
+          src={`${process.env.NEXT_PUBLIC_API}/static/media/videos_images/${cardData?.square_image}`}
+          alt={cardData?.title}
+          className={styles.cardImage}
+        />
+        <div className={styles.movieTitle}>{cardData?.title}</div>
+        <Link href={`/videos/${cardData?.public_id}`}>
+          <div
+            className={styles.cardBtn}
+            onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}
+          >
+            <PlayIcon hovered={hovered} />
+            <p>Watch</p>
+          </div>
+        </Link>
+      </div>
+    </Link>
+  );
+};
 
 export default SightsCard;
