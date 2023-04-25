3fdd1c7dc7627a0ef1836f49bbaab76fa2652b64
zidanegimiga <zidanegimigatemp@gmail.com>
2023-04-25 13:04:57 +0300

    fix: video playing issue

--- src/shared/Icons/Playback.tsx
+++ src/shared/Icons/Playback.tsx
@@ -5,9 +5,9 @@ const Play = ({ action }) => {
   return (
     <div onClick={action} className={styles.playIcon}>
       <svg
-        width="24"
-        height="24"
-        viewBox="0 0 48 48"
+        width="32"
+        height="32"
+        viewBox="0 0 64 64"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
         className={styles.playIconSvg}
