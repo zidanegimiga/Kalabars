beae0323e71b22c4d2aa5abf215e74879c68d61c
zidanegimiga <zidanegimigatemp@gmail.com>
2023-05-08 17:34:52 +0300

    styles

--- src/features/Sounds/Player/Player.tsx
+++ src/features/Sounds/Player/Player.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import styles from './Player.module.scss'
 import Image from 'next/image'
 import { Download, Forward, Play, Repeat, Rewind, Shuffle } from 'shared/Icons/SoundPlayerIcons';
 
 const Player = () => {
+  const [audioPlaying, setAudioPlaying] = useState<boolean>()
+  const playAudio = () => {
+
+  }
   return (
     <div className={styles.playerWrapper}>
         <div className={styles.audioMetadata}>
@@ -38,6 +42,7 @@ const Player = () => {
             </div>
             <div className={styles.progressArea}>
                 <div className={styles.progressBar}>
+                    <audio src={""}/>
                 </div>
                 <div className={styles.timer}> 
                     <span className={styles.current}>10:09</span>
