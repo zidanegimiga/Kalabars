9c066189be44028e6d434b14a97b1a1d6c70e920
henrymutua <mutuahenry116@gmail.com>
2023-06-08 16:34:02 +0300

    fixed the component dimensions

--- src/features/Sights/SightsCard/SightsCard.tsx
+++ src/features/Sights/SightsCard/SightsCard.tsx
@@ -16,48 +16,41 @@ const SightsCard = ({ cardData }) => {
   return (
     <Link href={`/video/${cardData?.public_id}`}>
       <div className={styles.categoryCardWrapper}>
-      <div className = {styles.innerCard}>
-        <img
+        
+        <div className = {styles.innerCard}>
+          <img
           src={`${process.env.NEXT_PUBLIC_API}/static/media/videos_images/${cardData?.square_image}`}
           alt={cardData?.title}
           className={styles.cardImage}
-
         />        
-
         
- 
         
+        <div className={styles.innerContainer}>
+          
 
-        <Link href={`/video/${cardData?.public_id}`}>
-        <div
+                        <div className={styles.movieTitle}>{cardData?.title}</div>
+                        <div className={styles.movieType}>Thriller  | 7 Minutes  | Directed by The shadow</div>
+                        <div className={styles.movieSummary}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</div>
+
+
+                        <Link href={`/video/${cardData?.public_id}`}>
+
+          <div
             className={styles.cardBtn}
             onMouseEnter={() => setHovered(true)}
             onMouseLeave={() => setHovered(false)}
           >
             <PlayIcon hovered={hovered} />
-            <p>Watch</p>
-          </div>
-        
-        </Link>
+            <p>Play Title</p>
 
-      </div>
-
-
-        <div className={styles.innerContainer}>
-          <div className={styles.movieTitle}>{cardData?.title}</div>
-          <Link href={`/video/${cardData?.public_id}`}>
-            <div
-              className={styles.cardBtn}
-              onMouseEnter={() => setHovered(true)}
-              onMouseLeave={() => setHovered(false)}
-            >
-              <PlayIcon hovered={hovered} />
-              <p>Watch</p>
             </div>
-          </Link>
 
-        </div>
+        </Link>
+                    </div>
 
+       </div>
+
+                    
       </div>
     </Link>
   );
