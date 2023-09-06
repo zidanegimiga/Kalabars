e44a60333f4dae4b2b8c5131196032ae7713aea1
zidanegimiga <zidanegimigatemp@gmail.com>
2023-09-06 12:19:53 +0300

    feat: search results can route to either audio or video pages

--- src/features/Sounds/Player/Player.tsx
+++ src/features/Sounds/Player/Player.tsx
@@ -50,6 +50,8 @@ const Player = () => {
     playAudio
   } = usePlaylist();
 
+  console.log("Current Audio: ", currentAudio)
+
   const audioRef = useRef(null);
   const timeline = useRef(null);
 
