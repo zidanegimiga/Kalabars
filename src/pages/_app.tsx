15f8f66311668c1f550e76f0b9e1af6fb0ec4b20
Zidane Gimiga <zidanegimigatemp@gmail.com>
2023-10-09 11:26:12 +0300

    Add analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,26 +8,30 @@ import { KalabarsContext, KalabarsProvider } from "../global/KalabarsContext";
 import { PlaylistProvider } from "global/AudioPlaylistContext";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import 'react-toastify/dist/ReactToastify.css'
+import { Analytics } from '@vercel/analytics/react';
 
 const App: FC = ({ Component, pageProps }: AppProps) => {
   return (
-    <div tabIndex={0}>
-    {/* <div tabIndex={0} onKeyDown={(e) => e.preventDefault()}> */}
-      <KalabarsProvider>
-        <NextNProgress
-          color="#ff5a5a"
-          startPosition={0.3}
-          stopDelayMs={200}
-          height={3}
-          showOnShallow={true}
-        />
-        <PlaylistProvider>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </PlaylistProvider>
-      </KalabarsProvider>
-    </div>
+    <>
+      <div tabIndex={0}>
+        {/* <div tabIndex={0} onKeyDown={(e) => e.preventDefault()}> */}
+        <KalabarsProvider>
+          <NextNProgress
+            color="#ff5a5a"
+            startPosition={0.3}
+            stopDelayMs={200}
+            height={3}
+            showOnShallow={true}
+          />
+          <PlaylistProvider>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </PlaylistProvider>
+        </KalabarsProvider>
+      </div>
+      <Analytics/>
+    </>
   );
 };
 
