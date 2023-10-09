528f598831d32e486007f34c00eddc7697561e78
Zidane Gimiga <zidanegimigatemp@gmail.com>
2023-10-09 11:29:06 +0300

    remove unnecessary comments

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,33 +1,3 @@
-// import { NextApiRequest, NextApiResponse } from "next";
-// import sqlite3 from "sqlite3";
-// import { open } from "sqlite";
-
-// export default async function handler(
-//   req: NextApiRequest,
-//   res: NextApiResponse
-// ) {
-//   let db = null;
-//   if (req.method === "POST") {
-//     const { email, password } = req.body;
-
-//     if (email && password) {
-//       const authHeader = btoa(email + ":" + password);
-//       const response = await fetch("https://content.kalabars.com/login", {
-//         headers: {
-//           "Content-Type": "application/json",
-//           Authorization: `Basic ${authHeader}`,
-//         },
-//       });
-//       const data = await response.json();
-//       res.status(200).json(data);
-//     } else {
-//       res.status(400).json({ error: "Invalid request body" });
-//     }
-//   } else {
-//     res.status(405).end();
-//   }
-// }
-
 import { NextApiRequest, NextApiResponse } from "next";
 import sqlite3 from "sqlite3";
 import { open } from "sqlite";
@@ -83,20 +53,3 @@ export default async function handler(
     res.status(405).end();
   }
 }
-
-
-// async function authenticateUser(username, password) {
-//   let db = null;
-
-//   // Check if the database instance has been initialized
-//   if (!db) {
-//     db = await open({
-//       filename: "userdatabase.db", // Specify the database file path
-//       driver: sqlite3.Database, // Specify the database driver (sqlite3 in this case)
-//     });
-//   }
-
-//   const sql = `SELECT * FROM users WHERE username = ? AND password = ?`;
-//   const user = await db.get(sql, username, password);
-//   return user;
-// }
