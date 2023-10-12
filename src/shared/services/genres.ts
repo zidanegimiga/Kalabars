eeef939d3d58d27c2285c702bbe3126a0f96e06a
zidanegimiga <zidanegimigatemp@gmail.com>
2023-10-12 11:57:49 +0300

    feat: create function to fetch genres

---
+++ src/shared/services/genres.ts
@@ -0,0 +1,29 @@
+import { BASE_API_URL } from "shared/constants";
+
+export type VideoGenresType = {
+  id: number;
+  landscape_image: string;
+  public_id: string;
+  slug: string;
+  square_image: string;
+  status: string;
+  title: string;
+};
+
+type GenresApiSuccessfulResponseType = {
+  result: VideoGenresType[];
+};
+
+export const getVideoGenres = async () => {
+    const response = await fetch(`${BASE_API_URL}/videos/genres`);
+    const responseJson = await response.json()
+    const data: GenresApiSuccessfulResponseType = responseJson.response
+    return data.result
+};
+
+export const getAudiooGenres = async () => {
+    const response = await fetch(`${BASE_API_URL}/audios/genres`);
+    const responseJson = await response.json()
+    const data: GenresApiSuccessfulResponseType = responseJson.response
+    return data.result
+};
