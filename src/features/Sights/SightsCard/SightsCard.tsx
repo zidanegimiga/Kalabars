82e11bfca99ddf1359efb54d3f6586e0ee6a1eeb
zidanegimiga <zidanegimigatemp@gmail.com>
2023-11-27 14:53:14 +0300

    style: remove left padding in sights card

--- src/features/Sights/SightsCard/SightsCard.tsx
+++ src/features/Sights/SightsCard/SightsCard.tsx
@@ -15,37 +15,39 @@ const SightsCard = ({ cardData }) => {
   }
 
   return (
-    <div className={styles.categoryCardWrapper}>
-      <Link href={`/video/${cardData?.public_id}`}>
-        <img
-          src={`${process.env.NEXT_PUBLIC_API}/static/media/videos_images/${cardData?.portrait_image}`}
-          alt={cardData?.title}
-          className={styles.cardImage}
-        />
-      </Link>
-      <div className={styles.innerContainer}>
-        <div className={styles.movieTitle}>{cardData?.title.toUpperCase()}</div>
-        <div className={styles.movieType}>
-          {cardData?.genres[0]?.title} | 
-          {/* {cardData?.duration} Minutes  */}
-          {" "}Directed by <span style={{fontWeight: "500"}}> {cardData?.creators_name}</span>
-        </div>
-        <div className={styles.movieSummary}>
-          {`${cardData?.description.slice(0, 100)}...`}
-        </div>
-
+    <div>
+      <div className={styles.categoryCardWrapper}>
         <Link href={`/video/${cardData?.public_id}`}>
-          <div
-            className={styles.cardBtn}
-            onMouseEnter={() => setHovered(true)}
-            onMouseLeave={() => setHovered(false)}
-          >
-            <div className={styles.playIcon}>
-              <PlayIcon hovered={hovered} initialColor={"#2b2b2b"} />
-            </div>
-            <p>Play Title</p>
-          </div>
+          <img
+            src={`${process.env.NEXT_PUBLIC_API}/static/media/videos_images/${cardData?.portrait_image}`}
+            alt={cardData?.title}
+            className={styles.cardImage}
+          />
         </Link>
+        <div className={styles.innerContainer}>
+          <div className={styles.movieTitle}>{cardData?.title.toUpperCase()}</div>
+          <div className={styles.movieType}>
+            {cardData?.genres[0]?.title} |
+            {/* {cardData?.duration} Minutes  */}
+            {" "}Directed by <span style={{ fontWeight: "500" }}> {cardData?.creators_name}</span>
+          </div>
+          <div className={styles.movieSummary}>
+            {`${cardData?.description.slice(0, 100)}...`}
+          </div>
+
+          <Link href={`/video/${cardData?.public_id}`}>
+            <div
+              className={styles.cardBtn}
+              onMouseEnter={() => setHovered(true)}
+              onMouseLeave={() => setHovered(false)}
+            >
+              <div className={styles.playIcon}>
+                <PlayIcon hovered={hovered} initialColor={"#2b2b2b"} />
+              </div>
+              <p>Play Title</p>
+            </div>
+          </Link>
+        </div>
       </div>
     </div>
   );
